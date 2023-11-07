2a5c1a6825ad64305b6a0ab6c0032fe63cdeb1e2
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-11-07 17:53:28 +0530

    create store and reducer

---
+++ 01_reduxToolkitTodo/src/features/todo/sliceTodo.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+// nanoid -> for generating unique id
+
+// assign with array or object, whatever you want to chose
+const initialState = {
+    todos: [{
+        id: 1,
+        text: "Welcome"
+    }]
+}
+
+// create slice(bigger version of reducer)
+export const todoSlice = createSlice({
+    name: "Todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload // remove through action
+            }
+            // take excess of state
+            state.todos.push(todo)
+
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+
+        }
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducers
